e8bef4dd1ed1ee96fbd4023d51bd9894415c5b9c
Aaron Longwell <a@adl.io>
2021-10-06 19:56:21 -0700

    Delete postgres_test file.

--- migrator_test.go
+++ migrator_test.go
@@ -36,6 +36,55 @@ func TestGetAppliedMigrationsErrorsWhenNoneExist(t *testing.T) {
 	})
 }
 
+func TestApplyMultistatementMigrations(t *testing.T) {
+	withEachDB(t, func(db *pgxpool.Pool) {
+		migrator := makeTestMigrator()
+		migrationSet1 := []*Migration{
+			{
+				ID: "2019-09-23 Create Artists and Albums",
+				Script: `
+		CREATE TABLE artists (
+			id SERIAL PRIMARY KEY,
+			name CHARACTER VARYING (255) NOT NULL DEFAULT ''
+		);
+		CREATE UNIQUE INDEX idx_artists_name ON artists (name);
+		CREATE TABLE albums (
+			id SERIAL PRIMARY KEY,
+			title CHARACTER VARYING (255) NOT NULL DEFAULT '',
+			artist_id INTEGER NOT NULL REFERENCES artists(id)
+		);
+		`,
+			},
+		}
+		err := migrator.Apply(db, migrationSet1)
+		if err != nil {
+			t.Error(err)
+		}
+
+		err = migrator.Apply(db, migrationSet1)
+		if err != nil {
+			t.Error(err)
+		}
+
+		migrationSet2 := []*Migration{
+			{
+				ID: "2019-09-24 Create Tracks",
+				Script: `
+		CREATE TABLE tracks (
+			id SERIAL PRIMARY KEY,
+			name CHARACTER VARYING (255) NOT NULL DEFAULT '',
+			artist_id INTEGER NOT NULL REFERENCES artists(id),
+			album_id INTEGER NOT NULL REFERENCES albums(id)
+		);`,
+			},
+		}
+		err = migrator.Apply(db, migrationSet2)
+		if err != nil {
+			t.Error(err)
+		}
+	})
+}
+
 func TestCommitOrRollbackRecoversErrorPanic(t *testing.T) {
 	m := makeTestMigrator()
 	defer func() {
